6c6f3617351b907dbc9ca30446a10494fa13e60c
Ana de Oliveira Rodrigues <ana.oliveira.rodrigues@gmail.com>
2021-09-22 12:30:27 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,51 +1,81 @@
 radio.onReceivedNumberDeprecated(function (receivedNumber) {
     if (receivedNumber == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
         Rover.MotorStopAll(MotorActions.Stop)
-        basic.showString("STOP")
     }
     if (receivedNumber == 2) {
-        basic.showString("R")
-        Rover.MotorRunDual(0, 50)
-        basic.pause(1000)
-        Rover.MotorStopAll(MotorActions.Stop)
-    }
-    if (receivedNumber == 3) {
-        basic.showString("L")
-        Rover.MotorRunDual(50, 0)
-        basic.pause(1000)
-        Rover.MotorStopAll(MotorActions.Stop)
-    }
-    if (receivedNumber == 4) {
-        basic.showString("BACKWARD")
-        Rover.MotorRunDual(-50, -50)
-        basic.pause(1000)
-        Rover.MotorStopAll(MotorActions.Stop)
-    }
-    if (receivedNumber == 5) {
-        basic.showString("FORWARD")
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
         Rover.MotorRunDual(50, 50)
         basic.pause(1000)
         Rover.MotorStopAll(MotorActions.Stop)
     }
+    if (receivedNumber == 3) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+        Rover.MotorRunDual(-50, -50)
+        basic.pause(1000)
+        Rover.MotorStopAll(MotorActions.Stop)
+    }
+    if (receivedNumber == 4) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+        Rover.MotorRunDual(0, 50)
+        basic.pause(1000)
+        Rover.MotorStopAll(MotorActions.Stop)
+    }
+    if (receivedNumber == 5) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        Rover.MotorRunDual(50, 0)
+        basic.pause(1000)
+        Rover.MotorStopAll(MotorActions.Stop)
+    }
 })
-// move forward
+// move backward
 input.onGesture(Gesture.LogoUp, function () {
-    radio.sendNumber(1)
+    radio.sendNumber(3)
 })
 // move left
 input.onGesture(Gesture.TiltLeft, function () {
-    radio.sendNumber(2)
+    radio.sendNumber(4)
 })
 // stop
 input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(5)
+    radio.sendNumber(1)
 })
 // move right
 input.onGesture(Gesture.TiltRight, function () {
-    radio.sendNumber(3)
+    radio.sendNumber(5)
 })
-// move backward
+// move forward
 input.onGesture(Gesture.LogoDown, function () {
-    radio.sendNumber(4)
+    radio.sendNumber(2)
 })
 radio.setGroup(1)
